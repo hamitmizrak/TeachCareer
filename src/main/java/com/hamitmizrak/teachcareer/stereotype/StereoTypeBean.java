2f36f14edb434bf5d179108442010b9a503c2a16
hamitmizrak <hamitmizrak@gmail.com>
2022-04-09 23:19:09 +0300

    @Stereotype

---
+++ src/main/java/com/hamitmizrak/teachcareer/stereotype/StereoTypeBean.java
@@ -0,0 +1,11 @@
+package com.hamitmizrak.teachcareer.stereotype;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@BenimStereoType
+@Data @NoArgsConstructor @AllArgsConstructor
+public class StereoTypeBean {
+    private String name="Merhabalar";
+}
