8d59dc8165d4b3a1a4fd9da3ed003891ce220cfa
hamitmizrak <hamitmizrak@gmail.com>
2022-04-09 22:26:58 +0300

    @New @Produces @Inject

---
+++ src/main/java/com/hamitmizrak/teachcareer/cdi/StudentDto.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.teachcareer.cdi;
+
+//Data Transfer Object =DTO
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentDto {
+
+    private Long studentId;
+    private String studentName;
+    private String studentSurname;
+}
